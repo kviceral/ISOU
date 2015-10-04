9dbdaddf917f8c2d55db61bcf2034e36e24b696b
kv <krisviceral@gmail.com>
2015-10-04 18:16:54 -0400

    Images showing up on grid

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -10,6 +10,11 @@
                 .success(function(photos) {
                     //TODO: put the metadata in another variable
                     $scope.photos = photos.photos.photo;
+                    
+                    for(var i=0; i < $scope.photos.length; i++){
+                        $scope.photos[i].urlDefault = $scope.constructDefaultImgLinks($scope.photos[i].farm, $scope.photos[i].server,
+                            $scope.photos[i].id, $scope.photos[i].secret);
+                    }
                 })
                 .error(function(data, status, headers, config) {
                     $log.log(data.error + ' ' + status);
@@ -27,6 +32,10 @@
                     $log.log(data.error + ' ' + status);
                     //TODO: throw something to UI
                 });
+        };
+
+        $scope.constructDefaultImgLinks = function(farm, server, id, secret){
+            return 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '.jpg';
         };
 
         init();
