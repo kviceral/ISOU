1fc14ecfc16807711562cd313ef587c7c67b9edf
kv <kviceral@users.noreply.github.com>
2015-10-07 00:05:11 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,19 @@
 (function() {
 
-    var app = angular.module('ISOUApp', []);
+    var app = angular.module('ISOUApp', [])
+                .constant("API", {
+                    "KEY": "a5e95177da353f58113fd60296e1d250",
+                    "USER_ID" : "132365033@N08",
+                    "BASE_URL" : "https://api.flickr.com/services/rest/",
+                    "FORMAT": "json",
+                    "NO_JSON_CALLBACK": 1
+                })
+                .constant("API_METHODS", {
+                    "GET_PUBLIC_PHOTOS" : "flickr.people.getPublicPhotos",
+                    "GET_PHOTO_INFO": "flickr.photos.getInfo",
+                    "GET_PHOTO_FAVORITES": "flickr.photos.getFavorites",
+                    "GET_PHOTO_SIZES": "flickr.photos.getSizes",
+                    "ADD_COMMENTS": "flickr.photos.comments.addComment",
+                    "GET_COMMENTS": "flickr.photos.comments.getList"
+                });
 }());
